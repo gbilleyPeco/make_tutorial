c8a08b2eab53637485c896071b64611d6abee69c
gbilleyPeco <119866794+gbilleyPeco@users.noreply.github.com>
2023-01-09 15:27:05 -0500

    creating example.

---
+++ pull_data.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+
+
+data = pd.DataFrame([
+    {'A': 1, 'B':1},
+    {'A': 2, 'B':2},
+    {'A': 3, 'B':1},
+    {'A': 4, 'B':2},
+    {'A': 5, 'B':1}
+    ])
+
+output_path = os.path.join('..', 'data')
+data.to_csv(ps.path.join(output_path, 'data.csv'))
