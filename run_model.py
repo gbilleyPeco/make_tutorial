af70e4bcdfc2d95a7e581c5b46860db2f9df079b
gbilleyPeco <119866794+gbilleyPeco@users.noreply.github.com>
2023-01-09 15:32:56 -0500

    Update run_model.py

--- run_model.py
+++ run_model.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 output_path = os.path.join('data')
-data = pd_read_csv(os.path.join(output_path, 'data.csv'))
+data = pd.read_csv(os.path.join(output_path, 'data.csv'))
 
 data_output = data*2
 data_output.to_csv(os.path.join(output_path, 'data_output.csv'))
